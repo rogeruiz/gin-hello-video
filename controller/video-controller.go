67635501184a40a0eaef315bbe02615e6e33e03c
Roger Steve Ruiz <hi@rog.gr>
2021-05-17 18:36:12 -0400

    Add in controllers so far...

---
+++ controller/video-controller.go
@@ -0,0 +1,46 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/rogeruiz/gin-hello-world/entity"
+	"github.com/rogeruiz/gin-hello-world/service"
+	"github.com/rogeruiz/gin-hello-world/validators"
+)
+
+type VideoController interface {
+	FindAll() []entity.Video
+	Save(ctx *gin.Context) error
+}
+
+type controller struct {
+	service service.VideoService
+}
+
+var validate *validator.Validate
+
+func New(service service.VideoService) VideoController {
+	validate = validator.New()
+	validate.RegisterValidation("is-cool", validators.ValidateCoolTitle)
+	return &controller{
+		service: service,
+	}
+}
+
+func (c *controller) FindAll() []entity.Video {
+	return c.service.FindAll()
+}
+
+func (c *controller) Save(ctx *gin.Context) error {
+	var video entity.Video
+	err := ctx.ShouldBindJSON(&video)
+	if err != nil {
+		return err
+	}
+	err = validate.Struct(video)
+	if err != nil {
+		return err
+	}
+	c.service.Save(video)
+	return nil
+}
