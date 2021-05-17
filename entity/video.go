16c3e29ef29e3146809c1071741b67eb15c0a06d
Roger Steve Ruiz <hi@rog.gr>
2021-05-17 18:36:38 -0400

    Add in entities so far...

---
+++ entity/video.go
@@ -0,0 +1,17 @@
+package entity
+
+// Person structure
+type Person struct {
+	FirstName string `json:"first_name" binding:"required"`
+	LastName  string `json:"last_name" binding:"required"`
+	Age       int8   `json:"age" binding:"gte=1,lte=130"`
+	Email     string `json:"email" validate:"required,email"`
+}
+
+// Video structure
+type Video struct {
+	Title       string `json:"title" binding:"min=2,max=30" validate:"is-cool"`
+	Description string `json:"description" binding:"max=60"`
+	URL         string `json:"url" binding:"required,url"`
+	Author      Person `json:"author" binding:"required"`
+}
