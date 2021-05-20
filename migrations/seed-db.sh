11542fe66c6a0fef3bf2d8192dd013e2259a5b51
Roger Steve Ruiz <hi@rog.gr>
2021-05-20 18:17:44 -0400

    Add in 4 more videos into dummy data

--- migrations/seed-db.sh
+++ migrations/seed-db.sh
@@ -22,3 +22,39 @@ make post-authored-video-auth \
   LASTNAME="Culinary" \
   AGE="36" \
   EMAIL="video@youtube.com"
+
+make post-authored-video-auth \
+  TITLE="Cool Loki in 30 Seconds" \
+  DESCRIPTION="Tom Hiddleston is on the clock." \
+  URL="https://www.youtube.com/embed/7LydHN2_eE4" \
+  FIRSTNAME="Tom" \
+  LASTNAME="Hiddleston" \
+  AGE="36" \
+  EMAIL="video@youtube.com"
+
+make post-authored-video-auth \
+  TITLE="Cool Paul Mooney Black Hollywood 1984" \
+  DESCRIPTION="All of scenes of comedian Paul Mooney in British documentary BLACK HOLLYWOOD." \
+  URL="https://www.youtube.com/embed/dX316_tNx5A" \
+  FIRSTNAME="Paul" \
+  LASTNAME="Mooney" \
+  AGE="79" \
+  EMAIL="video@youtube.com"
+
+make post-authored-video-auth \
+  TITLE="Cool BAJORNS: Cultural Index" \
+  DESCRIPTION="Learn about the Bajorn culture." \
+  URL="https://www.youtube.com/embed/qYWzGPI_SME" \
+  FIRSTNAME="Star" \
+  LASTNAME="Trek" \
+  AGE="36" \
+  EMAIL="video@youtube.com"
+
+make post-authored-video-auth \
+  TITLE="Cool You CAN do deep squats, my friend!" \
+  DESCRIPTION="Learn how to do deep squats." \
+  URL="https://www.youtube.com/embed/z3XQ7T4-abQ" \
+  FIRSTNAME="Hybrid" \
+  LASTNAME="Calisthenics" \
+  AGE="36" \
+  EMAIL="video@youtube.com"
